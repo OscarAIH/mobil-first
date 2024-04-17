64976ec83b13900dbbc0ca1a57fbc8f61dd4f0e5
Jonathan Walther <eaa23jwkh@students.eaaa.dk>
2024-04-17 13:12:19 +0200

    account knap

---
+++ js/script.js
@@ -0,0 +1,47 @@
+function toggleMenu() {
+  const menu = document.querySelector('.menu-overlay');
+  const accountMenu = document.querySelector('.account-menu-overlay');
+
+  if (menu.style.left === '0px') {
+      menu.style.left = '-100%';
+  } else {
+      if (accountMenu.style.right === '0px') {
+          accountMenu.style.right = '-60%'; 
+      }
+      menu.style.left = '0px';
+  }
+}
+
+  
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideshow");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+function toggleAccountMenu() {
+  const accountMenu = document.querySelector('.account-menu-overlay');
+  const menu = document.querySelector('.menu-overlay');
+
+  if (accountMenu.style.right === '0px') {
+      accountMenu.style.right = '-60%'; 
+  } else {
+      // Close burger menu if it's open
+      if (menu.style.left === '0px') {
+          menu.style.left = '-100%'; 
+      }
+      accountMenu.style.right = '0px'; 
+  }
+}
