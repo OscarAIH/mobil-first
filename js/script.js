0537b54a686c86340d8e669a7666326eb3f9b766
Jonathan Walther <eaa23jwkh@students.eaaa.dk>
2024-04-18 13:47:28 +0200

    nyt javascript

--- js/script.js
+++ js/script.js
@@ -1,51 +1,53 @@
+/* Menu positioner når åben og lukket */
+const MENU_CLOSED_LEFT = '-100%';
+const MENU_OPEN_LEFT = '0px';
+const MENU_CLOSED_RIGHT = '-60%';
+const MENU_OPEN_RIGHT = '0px';
 
-/* Burger Menu der slider ind fra venstre side  */
+/* Slideshow funktioner */
+const slideshow = {
+  slideIndex: 1,
+  showSlides: function(n) {
+    const slides = document.getElementsByClassName("slideshow");
+    if (n > slides.length) { this.slideIndex = 1; }
+    if (n < 1) { this.slideIndex = slides.length; }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slides[this.slideIndex - 1].style.display = "block";  
+  },
+  plusSlides: function(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+};
+
+/* Starter slideshow */
+slideshow.showSlides(slideshow.slideIndex);
+
+/* Togglefunktion af knapperne */
+function toggleElement(element, isOpen, closeValue, openValue, property) {
+  if (isOpen) {
+    element.style[property] = closeValue;
+  } else {
+    element.style[property] = openValue;
+  }
+}
+
+/* Udvidet menufunktioner */
 function toggleMenu() {
   const menu = document.querySelector('.menu-overlay');
   const accountMenu = document.querySelector('.account-menu-overlay');
-
-  /* Gør at menuerne ikke er åbne på samme tid */
-  if (menu.style.left === '0px') {
-      menu.style.left = '-100%';
-  } else {
-      if (accountMenu.style.right === '0px') {
-          accountMenu.style.right = '-60%'; 
-      }
-      menu.style.left = '0px';
+  toggleElement(menu, menu.style.left === MENU_OPEN_LEFT, MENU_CLOSED_LEFT, MENU_OPEN_LEFT, 'left');
+  if (menu.style.left === MENU_OPEN_LEFT && accountMenu.style.right === MENU_OPEN_RIGHT) {
+      accountMenu.style.right = MENU_CLOSED_RIGHT;
   }
 }
 
-  /* Slideshow funktion */
-  var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slideshow");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-}
-
-/* Profil Menu der slider ind fra højre side */
-function toggleAccountMenu() { 
+function toggleAccountMenu() {
   const accountMenu = document.querySelector('.account-menu-overlay');
   const menu = document.querySelector('.menu-overlay');
-
-  /* Gør at menuerne ikke er åbne på samme tid */
-  if (accountMenu.style.right === '0px') {
-      accountMenu.style.right = '-60%'; 
-  } else {
-      if (menu.style.left === '0px') {
-          menu.style.left = '-100%'; 
-      }
-      accountMenu.style.right = '0px'; 
+  toggleElement(accountMenu, accountMenu.style.right === MENU_OPEN_RIGHT, MENU_CLOSED_RIGHT, MENU_OPEN_RIGHT, 'right');
+  if (accountMenu.style.right === MENU_OPEN_RIGHT && menu.style.left === MENU_OPEN_LEFT) {
+      menu.style.left = MENU_CLOSED_LEFT;
   }
 }
